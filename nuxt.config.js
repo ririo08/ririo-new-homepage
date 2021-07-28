e90785885a9ad45e8409d804959d862ec131fe44
ririo08 <ririomail@gmail.com>
2021-07-29 00:02:40 +0900

    ⚙Global CSSの設定

--- nuxt.config.js
+++ nuxt.config.js
@@ -6,7 +6,7 @@ export default {
   head: {
     title: 'ririo-homepage',
     htmlAttrs: {
-      lang: 'en'
+      lang: 'ja'
     },
     meta: [
       { charset: 'utf-8' },
@@ -21,6 +21,8 @@ export default {
 
   // Global CSS: https://go.nuxtjs.dev/config-css
   css: [
+    '@/assets/css/bootstrap-reboot.min.css',
+    '@/assets/css/global.scss'
   ],
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
