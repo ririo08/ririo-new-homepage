4b520b54eb2e58f16226d0aae592db441f46c405
ririo08 <ririo08@users.noreply.github.com>
2021-07-30 01:23:18 +0000

    deploy: b6fbe28a33f04d0e6ab51e6a468269455ccb2d07

--- _nuxt/static/1627606270/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Fririo-new-homepage\u002F_nuxt\u002Fstatic\u002F1627606270",layout:"default",error:a,serverRendered:true,routePath:"\u002F",config:{_app:{basePath:"\u002Fririo-new-homepage\u002F",assetsPath:"\u002Fririo-new-homepage\u002F_nuxt\u002F",cdnURL:a}}}}(null));
