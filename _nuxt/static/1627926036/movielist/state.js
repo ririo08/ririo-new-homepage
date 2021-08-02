578b3ed739041b985cf0f81f6ba6614146138764
ririo08 <ririo08@users.noreply.github.com>
2021-08-02 17:47:36 +0000

    deploy: 330b23667758ef47b6db1ab6dabb56509a43be18

--- _nuxt/static/1627926036/movielist/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Fririo-new-homepage\u002F_nuxt\u002Fstatic\u002F1627926036",layout:"default",error:a,serverRendered:true,routePath:"\u002Fmovielist",config:{_app:{basePath:"\u002Fririo-new-homepage\u002F",assetsPath:"\u002Fririo-new-homepage\u002F_nuxt\u002F",cdnURL:a}}}}(null));
