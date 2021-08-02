a1b6c079fbc80ca003608a8fa9c7e5d571211593
ririo08 <ririo08@users.noreply.github.com>
2021-08-02 17:58:25 +0000

    deploy: d82e643e259159472a425eb90aaba25598c49117

--- _nuxt/static/1627926724/manifest.js
+++
@@ -1,1 +0,0 @@
-__NUXT_JSONP__("manifest.js", {routes:["\u002F","\u002Fhistory\u002Flive","\u002FpcSpec\u002Fdetail","\u002Fmovielist","\u002Fprofile","\u002Fhistory\u002Ftwitch","\u002FpcSpec\u002FoldGear","\u002Fhistory","\u002FpcSpec"]})
--- _nuxt/static/1627926724/movielist/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Fririotv.xyz\u002F_nuxt\u002Fstatic\u002F1627926724",layout:"default",error:a,serverRendered:true,routePath:"\u002Fmovielist",config:{_app:{basePath:"\u002Fririotv.xyz\u002F",assetsPath:"\u002Fririotv.xyz\u002F_nuxt\u002F",cdnURL:a}}}}(null));
