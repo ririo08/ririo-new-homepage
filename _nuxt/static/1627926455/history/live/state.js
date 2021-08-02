f2e0b88c0f6473cb1d4b0ffb6027ea096e5e7dbf
ririo08 <ririo08@users.noreply.github.com>
2021-08-02 17:52:05 +0000

    deploy: 193d16b0e46b2f8f84c20ce74fcb514b1e572f78

--- _nuxt/static/1627926455/history/live/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Fririotv.xyz\u002F_nuxt\u002Fstatic\u002F1627926455",layout:"default",error:a,serverRendered:true,routePath:"\u002Fhistory\u002Flive",config:{_app:{basePath:"\u002Fririotv.xyz\u002F",assetsPath:"\u002Fririotv.xyz\u002F_nuxt\u002F",cdnURL:a}}}}(null));
