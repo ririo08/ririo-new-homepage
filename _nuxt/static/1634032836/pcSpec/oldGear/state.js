6adc4b2140ebf2cbb9a6dd0d3753a09e01faabf0
ririo08 <ririo08@users.noreply.github.com>
2021-10-12 10:00:37 +0000

    deploy: 602829b60764337cc6b534dba978027ef950c716

---
+++ _nuxt/static/1634032836/pcSpec/oldGear/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1634032836",layout:"default",error:a,serverRendered:true,routePath:"\u002FpcSpec\u002FoldGear",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a}}}}(null));
