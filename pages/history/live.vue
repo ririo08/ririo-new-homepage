b19189ee129c487de6ea59ca98f75b7cdf1123d3
ririo08 <ririomail@gmail.com>
2022-01-03 01:33:06 +0900

    🔧2021年の活動記録を追記しました。

--- pages/history/live.vue
+++ pages/history/live.vue
@@ -4,12 +4,12 @@
       <h1 class="globalBox-title">YouTube Live history</h1>
       <p class="history-details_p">
         活動の記録です。現在{{ date }}日目（{{ year }}年目）！<br>
-				（）付きの配信時間はYouTubeではなくTwitchでの配信を含めた配信時間となります。
+				（）付きの配信時間はYouTubeではなくTwitchの配信時間となります。
       </p>
     </div>
     <div class="historyList">
       <ul class="historyList-ul">
-          <!-- <li class="historyList-li historyRed"><span class="historyList-li_date">2020</span>再生回数：6,758回 投稿数：135本</li> -->
+          <li class="historyList-li historyRed"><span class="historyList-li_date">2021</span>配信時間：875時間（522時間）計1397時間 視聴回数：13,295回</li>
           <li class="historyList-li"><span class="historyList-li_date">2021/10/31</span>総再生数10,000回突破</li>
           <li class="historyList-li"><span class="historyList-li_date">2021/10/02</span>累計配信500時間突破</li>
           <li class="historyList-li"><span class="historyList-li_date">2021/06/12</span>累計配信100時間突破</li>
