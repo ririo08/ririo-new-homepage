2c45e5e9ab9b9ff6e2fc6da09b471a1433f11486
ririo08 <ririo08@users.noreply.github.com>
2022-06-21 12:54:12 +0000

    deploy: cbf77bc0c93256c02afb5264447c68c38070f1b5

---
+++ _nuxt/static/1655816051/profile/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Fhomepage\u002F_nuxt\u002Fstatic\u002F1655816051",layout:"default",error:a,serverRendered:true,routePath:"\u002Fprofile",config:{_app:{basePath:"\u002Fhomepage\u002F",assetsPath:"\u002Fhomepage\u002F_nuxt\u002F",cdnURL:a}}}}(null));
