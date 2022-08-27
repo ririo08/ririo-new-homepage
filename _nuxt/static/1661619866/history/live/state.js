62c836f4a504901626497444f0201c4db82a01f8
ririo08 <ririo08@users.noreply.github.com>
2022-08-27 17:04:27 +0000

    deploy: c022219f26decd8114aa80f5d5e383e04eb95565

---
+++ _nuxt/static/1661619866/history/live/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Fhomepage\u002F_nuxt\u002Fstatic\u002F1661619866",layout:"default",error:a,serverRendered:true,routePath:"\u002Fhistory\u002Flive",config:{_app:{basePath:"\u002Fhomepage\u002F",assetsPath:"\u002Fhomepage\u002F_nuxt\u002F",cdnURL:a}}}}(null));
