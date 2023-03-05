ecb3ea9d086e8089b9bdade6ba230ad19f9e2ca8
ririo08 <ririomail@gmail.com>
2023-03-05 18:53:05 +0900

    feat: 活動記録の取得方法をdbに変更

--- pages/history/live.vue
+++ pages/history/live.vue
@@ -9,7 +9,7 @@
     </div>
     <div class="historyList">
       <ul class="historyList-ul">
-        <span v-for="item in this.lockerStatus" :key="item.Id">
+        <span v-for="item in list" :key="item.Id">
           <li class="historyList-li historyRed" v-if="item.IsYear">
             <span class="historyList-li_date">
               {{ item.Date }}
@@ -28,38 +28,12 @@
 
 <script>
 export default {
-  asyncData({ $axios }) {
-    return $axios
-      .get(
-        'https://sheets.googleapis.com/v4/spreadsheets/1ZxI-QHaLyLTlERtTloxvnTFXO02onsMriR_UVSam6yE/values/history-live?key=AIzaSyAe5CX3okoCZT00uxT2ckdRv6oUJ3YNj9o'
-      )
-      .then((res) => {
-        const jsonData = res.data.values
-        const key1 = jsonData[0][0]
-        const key2 = jsonData[0][1]
-        const key3 = jsonData[0][2]
-        const key4 = jsonData[0][3]
-        const key5 = jsonData[0][4]
-
-        let historyList = []
-        for (let i = 1; i < jsonData.length; i++) {
-          let obj = {}
-          obj[key1] = jsonData[i][0]
-          obj[key2] = jsonData[i][1]
-          obj[key3] = jsonData[i][2]
-          obj[key4] = jsonData[i][3]
-          obj[key5] = jsonData[i][4]
-          historyList.push(obj)
-        }
-        historyList.reverse()
-        return { lockerStatus: historyList }
-      })
-  },
   data() {
-    return {
-      date: 0,
-      year: 0,
-    }
+    return { list: [], date: 0, year: 0 }
+  },
+  async fetch() {
+    const res = await this.$axios.$get('https://ririo.dev/db/history-live.json')
+    this.list = res.reverse()
   },
   mounted: function () {
     let a = Date.parse('2019/09/09')
