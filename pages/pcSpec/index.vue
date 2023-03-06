7a3cc2bd8078dec423e44baae7e81bb57210c8c3
ririo08 <ririomail@gmail.com>
2023-03-06 21:53:54 +0900

    feat: ガジェット一覧のDB化

--- pages/pcSpec/index.vue
+++ pages/pcSpec/index.vue
@@ -25,16 +25,14 @@
         <img src="~assets/img/pcSpec/4thPC.png" alt="PC4世代目グラフ" />
       </div>
     </section>
-    <section class="pcSpec-list">
+    <section v-for="item in list" class="pcSpec-list" :key="item.ProductName">
       <div class="pcSpec-description">
-        <h2>☆Monitor</h2>
-        <div class="amazlet-box">
+        <h2 v-if="item.IsCategory === 'TRUE'">☆{{ item.ProductName }}</h2>
+        <div v-else class="amazlet-box">
           <div class="amazlet-image">
-            <a href="https://amzn.to/32coaxm" name="amazletlink" target="_blank"
-              ><img
-                src="https://images-fe.ssl-images-amazon.com/images/I/51ZUUFFJGTL._SL160_.jpg"
-                alt="ASUSゲーミングモニター 24.5インチ"
-            /></a>
+            <a :href="item.Link" name="amazletlink" target="_blank">
+              <img :src="item.ImgLink" alt="商品画像" />
+            </a>
           </div>
           <div class="amazlet-info">
             <div class="amazlet-name">
@@ -42,354 +40,22 @@
                 href="https://amzn.to/32coaxm"
                 name="amazletlink"
                 target="_blank"
-                >ASUS - ゲーミングモニター 24.5インチ VG258QR-J</a
               >
-              <div class="amazlet-powered-date">posted at 2020.08.22</div>
-            </div>
-            <div class="amazlet-detail">
-              Asustek (2019-11-5)<br />売り上げランキング: 160<br />
-            </div>
-            <div class="amazlet-sub-info">
-              <div class="amazlet-link">
-                <a
-                  href="https://amzn.to/32coaxm"
-                  name="amazletlink"
-                  target="_blank"
-                  >Amazon.co.jpで詳細を見る</a
-                >
+                {{ item.ProductName }}
+              </a>
+              <div class="amazlet-powered-date">
+                posted at {{ item.PostedDate }}
               </div>
             </div>
-          </div>
-        </div>
-        <div class="amazlet-box">
-          <div class="amazlet-image">
-            <a href="https://amzn.to/3gnPPlg" name="amazletlink" target="_blank"
-              ><img
-                src="https://images-fe.ssl-images-amazon.com/images/I/51ZumJVo-HL._SL160_.jpg"
-                alt="ASUS ゲーミングモニター24型 フルHDディスプレイ (応答速度1ms / HDMI×2ポート,D-sub/スピーカー内蔵 / 3年保証) VX248H"
-            /></a>
-          </div>
-          <div class="amazlet-info">
-            <div class="amazlet-name">
-              <a
-                href="https://amzn.to/3gnPPlg"
-                name="amazletlink"
-                target="_blank"
-                >ASUS - ゲーミングモニター24型 FHDディスプレイ VX248H</a
-              >
-              <div class="amazlet-powered-date">posted at 19.10.05</div>
-            </div>
             <div class="amazlet-detail">
-              Asustek (2015-04-04)<br />売り上げランキング: 1,401<br />
+              {{ item.BrandName }}<br />
+              売り上げランキング: {{ item.Ranking }}<br />
             </div>
             <div class="amazlet-sub-info">
               <div class="amazlet-link">
-                <a
-                  href="https://amzn.to/3gnPPlg"
-                  name="amazletlink"
-                  target="_blank"
-                  >Amazon.co.jpで詳細を見る</a
-                >
-              </div>
-            </div>
-          </div>
-        </div>
-        <div class="amazlet-box">
-          <div class="amazlet-image">
-            <a href="https://amzn.to/2M45QyY" name="amazletlink" target="_blank"
-              ><img
-                src="https://images-fe.ssl-images-amazon.com/images/I/41CTMUIpuTL._SL160_.jpg"
-                alt="Acer モニター ディスプレイ KA220HQbid 21.5インチ/フルHD/5ms/HDMI端子対応"
-            /></a>
-          </div>
-          <div class="amazlet-info">
-            <div class="amazlet-name">
-              <a
-                href="https://amzn.to/2M45QyY"
-                name="amazletlink"
-                target="_blank"
-                >Acer - モニター21.5型 FHDディスプレイ KA220HQbid</a
-              >
-              <div class="amazlet-powered-date">posted at 19.10.05</div>
-            </div>
-            <div class="amazlet-detail">
-              日本エイサー (2015-06-30)<br />売り上げランキング: 6,734<br />
-            </div>
-            <div class="amazlet-sub-info">
-              <div class="amazlet-link">
-                <a
-                  href="https://amzn.to/2M45QyY"
-                  name="amazletlink"
-                  target="_blank"
-                  >Amazon.co.jpで詳細を見る</a
-                >
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-    </section>
-    <section class="pcSpec-list">
-      <div class="pcSpec-description">
-        <h2>☆Keyboard</h2>
-        <div class="amazlet-box">
-          <div class="amazlet-image">
-            <a href="https://amzn.to/2A8JUQq" name="amazletlink" target="_blank"
-              ><img
-                src="https://images-fe.ssl-images-amazon.com/images/I/41dkpPurp6L._SL160_.jpg"
-                alt="【国内正規品】 SteelSeries 日本語配列 テンキーレス ゲーミング キーボード Apex Pro TKL JP OmniPointスイッチ 有機EL ディスプレイ搭載"
-            /></a>
-          </div>
-          <div class="amazlet-info">
-            <div class="amazlet-name">
-              <a
-                href="https://amzn.to/2A8JUQq"
-                name="amazletlink"
-                target="_blank"
-                >SteelSeries - Apex Pro TKL JP</a
-              >
-              <div class="amazlet-powered-date">posted at 19.12.21</div>
-            </div>
-            <div class="amazlet-detail">
-              SteelSeries (2019-12-18)<br />売り上げランキング: 1,498<br />
-            </div>
-            <div class="amazlet-sub-info">
-              <div class="amazlet-link">
-                <a
-                  href="https://amzn.to/2A8JUQq"
-                  name="amazletlink"
-                  target="_blank"
-                  >Amazon.co.jpで詳細を見る</a
-                >
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-    </section>
-    <section class="pcSpec-list">
-      <div class="pcSpec-description">
-        <h2>☆Mouse</h2>
-        <div class="amazlet-box">
-          <div class="amazlet-image">
-            <a href="https://amzn.to/2YnMzyK" name="amazletlink" target="_blank"
-              ><img
-                src="https://images-fe.ssl-images-amazon.com/images/I/316pb9RKcwL._SL160_.jpg"
-                alt="Logicool G ゲーミングマウス 無線 G703h HEROセンサー LIGHTSPEED ワイヤレス エルゴノミクス LIGHTSYNC RGB POWERPLAY ワイヤレス充電 国内正規品"
-            /></a>
-          </div>
-          <div class="amazlet-info">
-            <div class="amazlet-name">
-              <a
-                href="https://amzn.to/2YnMzyK"
-                name="amazletlink"
-                target="_blank"
-                >Logicool G - ゲーミングマウス 無線 G703h</a
-              >
-              <div class="amazlet-powered-date">posted at 2020.08.22</div>
-            </div>
-            <div class="amazlet-detail">
-              Logicool G(ロジクール G) (2019-6-18)<br />売り上げランキング:
-              29<br />
-            </div>
-            <div class="amazlet-sub-info">
-              <div class="amazlet-link">
-                <a
-                  href="https://amzn.to/2YnMzyK"
-                  name="amazletlink"
-                  target="_blank"
-                  >Amazon.co.jpで詳細を見る</a
-                >
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-    </section>
-    <section class="pcSpec-list">
-      <div class="pcSpec-description">
-        <h2>☆Mousepad</h2>
-        <div class="amazlet-box">
-          <div class="amazlet-image">
-            <a href="https://amzn.to/3gqJxBf" name="amazletlink" target="_blank"
-              ><img
-                src="https://images-fe.ssl-images-amazon.com/images/I/31uODLVSr0L._SL160_.jpg"
-                alt="【国内正規品】SteelSeries QcK マウスパッド 63003"
-            /></a>
-          </div>
-          <div class="amazlet-info">
-            <div class="amazlet-name">
-              <a
-                href="https://amzn.to/3gqJxBf"
-                name="amazletlink"
-                target="_blank"
-                >SteelSeries - QcK 63003</a
-              >
-              <div class="amazlet-powered-date">posted at 2020-12-25</div>
-            </div>
-            <div class="amazlet-detail">
-              SteelSeries(2007-05-01) <br />売り上げランキング: 6<br />
-            </div>
-            <div class="amazlet-sub-info">
-              <div class="amazlet-link">
-                <a
-                  href="https://amzn.to/3gqJxBf"
-                  name="amazletlink"
-                  target="_blank"
-                  >Amazon.co.jpで詳細を見る</a
-                >
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-    </section>
-    <section class="pcSpec-list">
-      <div class="pcSpec-description">
-        <h2>☆A I/F - Mixer</h2>
-        <div class="amazlet-box">
-          <div class="amazlet-image">
-            <a href="https://amzn.to/2TCUGp1" name="amazletlink" target="_blank"
-              ><img
-                src="https://images-fe.ssl-images-amazon.com/images/I/513trZ-1KqL._SL160_.jpg"
-                alt="YAMAHA ウェブキャスティングミキサー 3チャンネル 「初音ミクおためしセット」 AG03-MIKU"
-            /></a>
-          </div>
-          <div class="amazlet-info">
-            <div class="amazlet-name">
-              <a
-                href="https://amzn.to/2TCUGp1"
-                name="amazletlink"
-                target="_blank"
-                >YAMAHA - AG03-MIKU ウェブキャスティングミキサー 3チャンネル</a
-              >
-              <div class="amazlet-powered-date">posted at 19.10.05</div>
-            </div>
-            <div class="amazlet-detail">
-              ヤマハ(YAMAHA) (2015-05-31)<br />売り上げランキング: 3,405<br />
-            </div>
-            <div class="amazlet-sub-info">
-              <div class="amazlet-link">
-                <a
-                  href="https://amzn.to/2TCUGp1"
-                  name="amazletlink"
-                  target="_blank"
-                  >Amazon.co.jpで詳細を見る</a
-                >
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-    </section>
-    <section class="pcSpec-list">
-      <div class="pcSpec-description">
-        <h2>☆Mic</h2>
-        <div class="amazlet-box">
-          <div class="amazlet-image">
-            <a href="https://amzn.to/3efMnay" name="amazletlink" target="_blank"
-              ><img
-                src="https://images-fe.ssl-images-amazon.com/images/I/41rtSiyXc6L._SL160_.jpg"
-                alt="audio-technica オーディオテクニカ コンデンサーマイクロホン AT2020 生放送・録音・ポッドキャスト・実況・DTM"
-            /></a>
-          </div>
-          <div class="amazlet-info">
-            <div class="amazlet-name">
-              <a
-                href="https://amzn.to/3efMnay"
-                name="amazletlink"
-                target="_blank"
-                >audio-technica - AT2020 コンデンサーマイクロホン</a
-              >
-              <div class="amazlet-powered-date">posted at 19.10.05</div>
-            </div>
-            <div class="amazlet-detail">
-              Audio Technica(オーディオテクニカ) <br />売り上げランキング: 54<br />
-            </div>
-            <div class="amazlet-sub-info">
-              <div class="amazlet-link">
-                <a
-                  href="https://amzn.to/3efMnay"
-                  name="amazletlink"
-                  target="_blank"
-                  >Amazon.co.jpで詳細を見る</a
-                >
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-    </section>
-    <section class="pcSpec-list">
-      <div class="pcSpec-description">
-        <h2>☆Headphone</h2>
-        <div class="amazlet-box">
-          <div class="amazlet-image">
-            <a href="https://amzn.to/3d26VmF" name="amazletlink" target="_blank"
-              ><img
-                src="https://images-fe.ssl-images-amazon.com/images/I/41FEX5t40LL._SL160_.jpg"
-                alt="【国内正規品】密閉型 ゲーミングヘッドセット SteelSeries Arctis 5 Black (2019 Edition) 61504"
-            /></a>
-          </div>
-          <div class="amazlet-info">
-            <div class="amazlet-name">
-              <a
-                href="https://amzn.to/3d26VmF"
-                name="amazletlink"
-                target="_blank"
-                >SteelSeries - Arctis 5 Black (2019 Edition) 61504</a
-              >
-              <div class="amazlet-powered-date">posted at 20.05.27</div>
-            </div>
-            <div class="amazlet-detail">
-              SteelSeries<br />売り上げランキング: 858<br />
-            </div>
-            <div class="amazlet-sub-info">
-              <div class="amazlet-link">
-                <a
-                  href="https://amzn.to/3d26VmF"
-                  name="amazletlink"
-                  target="_blank"
-                  >Amazon.co.jpで詳細を見る</a
-                >
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-    </section>
-    <section class="pcSpec-list">
-      <div class="pcSpec-description">
-        <h2>☆Capture Card</h2>
-        <div class="amazlet-box">
-          <div class="amazlet-image">
-            <a href="https://amzn.to/2A9JbOU" name="amazletlink" target="_blank"
-              ><img
-                src="https://images-fe.ssl-images-amazon.com/images/I/41T1nc8d10L._SL160_.jpg"
-                alt="Elgato Game Capture HD60 Pro - ゲームキャプチャー PS4, Xbox One and Nintendo Switch対応 1GC109901002 【 日本正規代理店品 】"
-            /></a>
-          </div>
-          <div class="amazlet-info">
-            <div class="amazlet-name">
-              <a
-                href="https://amzn.to/2A9JbOU"
-                name="amazletlink"
-                target="_blank"
-                >Elgato - Game Capture HD60 Pro</a
-              >
-              <div class="amazlet-powered-date">posted at 20.05.27</div>
-            </div>
-            <div class="amazlet-detail">
-              Elgato gaming (CORSAIR)<br />売り上げランキング: 167<br />
-            </div>
-            <div class="amazlet-sub-info">
-              <div class="amazlet-link">
-                <a
-                  href="https://amzn.to/2A9JbOU"
-                  name="amazletlink"
-                  target="_blank"
-                  >Amazon.co.jpで詳細を見る</a
-                >
+                <a :href="item.Link" name="amazletlink" target="_blank">
+                  Amazon.co.jpで詳細を見る
+                </a>
               </div>
             </div>
           </div>
@@ -399,11 +65,22 @@
   </div>
 </template>
 
+<script>
+export default {
+  data() {
+    return { list: [] }
+  },
+  async fetch() {
+    const res = await this.$axios.$get('https://ririo.dev/db/pc-gadget.json')
+    this.list = res
+  },
+}
+</script>
+
 <style lang="scss">
 $themeColor: orange;
 .pcSpec {
   &-list {
-    margin-top: 40px;
     display: flex;
     justify-content: space-between;
   }
@@ -418,6 +95,7 @@ $themeColor: orange;
     }
 
     h2 {
+      margin-top: 20px;
       font-size: 16px;
       font-weight: bold;
       padding-bottom: 10px;
@@ -449,6 +127,8 @@ $amazletLinks: #915500;
   }
 
   &-image {
+    background-color: #fff;
+    display: flex;
     width: 160px;
     height: 160px;
     & a {
