8bc0b0c699ef602890727f8ad2a8fe4971293459
ririo08 <ririo08@users.noreply.github.com>
2024-02-09 01:31:17 +0900

    fix: ESLintを動くように…

--- eslint.config.js
+++ eslint.config.js
@@ -1,13 +1,14 @@
+// eslint.config.js
 import antfu from '@antfu/eslint-config'
 
 export default antfu({
   stylistic: {
     indent: 2, // 4, or 'tab'
     quotes: 'single', // or 'double'
-    semi: false
+    semi: false,
   },
   typescript: {
-    tsconfigPath: "tsconfig.json"
+    tsconfigPath: 'tsconfig.json',
   },
   vue: true,
 })
